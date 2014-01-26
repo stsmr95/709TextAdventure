c05eb4f56747dfe1aacc169cc8ba5dbe93cf1349
Alex Hawker <pumpkinpi64@gmail.com>
2014-01-26 00:13:22 -0800

    Included client loading socket.io, made server send initial scene

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server); /* socket.io for sending scenes to user */
 var fs = require('fs') /* file stream for opening files */
+var STARTING_SCENE="Start";
 
 
 /* turn on console logging for server*/
@@ -18,22 +19,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* socket.io */
 io.sockets.on('connection', function (socket) {
 
+  sendScene([STARTING_SCENE]);
+
    /* when the server receives a request for a new scene */
    socket.on('newScene', function(data){
-          /* Read code from text file */
-          /* first check for invalid input */
-          if (!data[0].contains("\\") && !data[0].contains("/"))
-          {
-             /* try to read the file */
-             fs.readFile("public/"+data[0], 'utf8', 
-                         function(err, result) { 
-                            if (err) throw err; /* problem */
-
-                            /* send to client */
-                            socket.emit('fPos', [data]);
-                         });
-          }
-      });
+       sendScene(data);
+   });
 });
 
 // Route for everything else - 404 error.
@@ -42,3 +33,21 @@ io.sockets.on('connection', function (socket) {
    });
 
 server.listen(8080);
+
+function sendScene( data )
+{
+    /* Read code from text file */
+    /* first check for invalid input */
+    if (!data[0].contains("\\") && !data[0].contains("/"))
+    {
+       /* try to read the file */
+       fs.readFile("public/"+data[0]+".txt", 'utf8', 
+                   function(err, result) { 
+                      if (err) throw err; /* problem */
+
+                      /* send to client */
+                      socket.emit('fPos', [data]);
+                   });
+    }
+
+}
